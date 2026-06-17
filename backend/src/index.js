bac88c0ca7b2821fd5f1b432c29b35aa82ffc3d2
VickY-1997 <vigneshkumar3497@gmail.com>
2026-06-17 11:58:37 +0530

    db setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,24 @@
-import express from 'express'
-import "dotenv/config"
-const app = express()
-const PORT = process.env.PORT || 2026
+import express from "express";
+import "dotenv/config";
+import { connectDb } from "./lib/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import cors from "cors";
+import dns from "dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
-app.get('/', (req,res) => {
-    res.send('hiii')
-})
+const app = express();
+const PORT = process.env.PORT || 2026;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+app.use(express.json());
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
+app.use(clerkMiddleware());
+
+app.get("/", (req, res) => {
+  res.send("Hi");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server connected port ${PORT}`)
-}) 
+  connectDb();
+  console.log(`Server connected port ${PORT}`);
+});
